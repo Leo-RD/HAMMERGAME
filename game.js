b6748662db4054832384e93e9704110042d10ef0
leopold rouxdecorzent <leopold.rouxdecorzent@saintmichelannecy.fr>
2025-05-19 14:00:07 +0200

    Increase gravity in launchConfetti2 function for enhanced confetti effect

--- game.js
+++ game.js
@@ -143,7 +143,7 @@ function launchConfetti2() {
         shapes: ['circle', 'square', 'circle'],
         scalar: 1.2,
         ticks: 200,
-        gravity: 0.6,
+        gravity: 100,
         drift: 0.05,
     });
 }
